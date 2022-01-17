02db2281f28d40a025d903dcac71e777c8250c68
AayushMohan <aayushmohan1702@gmail.com>
2022-01-17 19:24:34 +0530

    commit

--- components/Login.tsx
+++ components/Login.tsx
@@ -11,8 +11,8 @@ export const Login = () => {
     {/* Login button */}
    </div>
 
-   <div>
-    <Image src="https://links.papareact.com/55n" layout="fill"/>
+   <div className="w-full h-screen">
+    <Image src="https://links.papareact.com/55n" layout="fill" objectFit="cover"/>
    </div>
 
   </div>
