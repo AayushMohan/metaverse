2a109dfc96a804d0d7c61476648ffd8709250425
AayushMohan <aayushmohan1702@gmail.com>
2022-01-27 20:15:40 +0530

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
-import Head from 'next/head'
-import { Login } from '../components/Login'
+import Head from "next/head";
+import { Login } from "../components/Login";
 import { useMoralis } from "react-moralis";
 
-
 export default function Home() {
+  const { isAuthenticated, logout } = useMoralis();
 
-  const {isAuthenticated, logout} = useMoralis();
-
-
-  if (!isAuthenticated) return <Login />
+  if (!isAuthenticated) return <Login />;
 
   return (
     <div className="h-screen">
@@ -17,8 +14,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <h1>Welcome to the APP</h1> 
-    <button onClick={logout}>Logout</button>
+      <h1>Welcome to the APP</h1>
+      <button onClick={logout}>Logout</button>
     </div>
-  )
+  );
 }
