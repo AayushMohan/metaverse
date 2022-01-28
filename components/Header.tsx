07fee8321b0a1a54ee439b081f263da03df130d1
AayushMohan <aayushmohan1702@gmail.com>
2022-01-29 00:32:05 +0530

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { useMoralis } from "react-moralis";
+import Avatar from "./Avatar";
 
 const Header = () => {
   const { user } = useMoralis();
@@ -7,13 +8,22 @@ const Header = () => {
   return (
     <div>
       <div>
-        <div className="relative h-24 w-24 mx-auto">
+        <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
           <Image
             layout="fill"
             objectFit="cover"
             className="rounded-full"
             src="https://avatars.githubusercontent.com/u/66319691?v=4"
           />
+        </div>
+
+        <div>
+          {/* Avatar */}
+          <Avatar />
+
+          {/* Welcome Message */}
+          {/* username */}
+          {/* Change username component */}
         </div>
       </div>
     </div>
