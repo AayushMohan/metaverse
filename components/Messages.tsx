3654dfd2739ef57ce952c3ba03930d0b0b79167d
AayushMohan <aayushmohan1702@gmail.com>
2022-01-29 20:05:21 +0530

    commit

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -1,20 +1,45 @@
 import React, { useRef } from "react";
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
+import Message from "./Message";
 import SendMessage from "./SendMessage";
+
+const MINS_DURATION = 15;
 
 const Messages = () => {
   const { user } = useMoralis();
   const endOfMessageRef = useRef(null);
+
+  const { data, loading, error } = useMoralisQuery(
+    "Messages",
+    (query) =>
+      query
+        .ascending("createdAt")
+        .greaterThan(
+          "createdAt",
+          new Date(Date.now() - 1000 * 60 * MINS_DURATION)
+        ),
+    [],
+    {
+      live: true,
+    }
+  );
+
+  console.log(data);
+
   return (
     <div className="pb-56">
-      <div>
+      <div className="my-5">
         <ByMoralis
           variant="dark"
           style={{ marginLeft: "auto", marginRight: "auto" }}
         />
       </div>
 
-      <div>{/* Each Message */}</div>
+      <div>
+        {data.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
 
       <div className="flex justify-center">
         <SendMessage endOfMessagesRef={endOfMessageRef} />
