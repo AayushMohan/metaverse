539473e0246559aa65fc439aa53a28d80a017546
AayushMohan <aayushmohan1702@gmail.com>
2022-01-29 12:09:07 +0530

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   const { user } = useMoralis();
 
   return (
-    <div className="sticky top-0 p-5 z-50 text-pink-500">
+    <div className="sticky top-0 p-5 z-50 bg-black shadow-sm text-pink-500 border-b-2 border-pink-700">
       <div className="grid grid-cols-5 lg:grid-cols-6 items-end lg:items-center">
         <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
           <Image
