e632ca1d20ded372f94fe0417578f574e9cf9909
AayushMohan <aayushmohan1702@gmail.com>
2022-01-29 20:32:47 +0530

    commit

--- components/Message.tsx
+++ components/Message.tsx
@@ -7,8 +7,18 @@ const Message = ({ message }) => {
   const isUserMessage = message.get("ethAddress") === user.get("ethAddress");
 
   return (
-    <div>
-      <div>
+    <div
+      className={`flex items-end space-x-2 relative ${
+        isUserMessage && "justify-end"
+      }`}
+    >
+      <div
+        className={`flex space-x-4 p-3 rounded-lg ${
+          isUserMessage
+            ? "rounded-br-none bg-pink-300"
+            : "rounded-bl-none bg-blue-400"
+        }`}
+      >
         <p>{message.get("message")}</p>
       </div>
     </div>
